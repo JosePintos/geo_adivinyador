9fb3d4eee2fe11b2a9184b4f78793eae49cc1698
JosePintos <jpintos.124@gmail.com>
2024-05-22 01:51:47 -0300

    change in venv and added requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import logging
 
 app = Flask(__name__)
 CORS(app)
@@ -8,15 +9,19 @@ TOKEN = "edbf7d4ca8e928"
 
 
 def get_geolocation(ip_address):
-    query = f"https://ipinfo.io/181.116.61.30?token={TOKEN}"
+    query = f"https://ipinfo.io/{ip_address}?token={TOKEN}"
     res = requests.get(query)
     return res.json()
 
 
 @app.route("/api/get_ip", methods=["GET"])
 def get_ip():
-    ip_address = request.remote_addr
+    ip_address = request.headers.get("X-Forwarded-For", request.remote_addr)
+    if ip_address:
+        ip_address = ip_address.split(",")[0].strip()
+
     geolocation_data = get_geolocation(ip_address)
+    app.logger.info(ip_address)
     return jsonify(geolocation_data)
 
 
